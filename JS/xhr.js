b4faa48afcaaf9355f996261db8c0d6404b54dc3
Lucas Gabriel/Luke <lg.lajedo2@gmail.com>
2022-08-03 10:50:51 -0300

    primeira requisição

--- JS/xhr.js
+++ JS/xhr.js
@@ -1,3 +1,12 @@
+// https://jsonplaceholder.typicode.com/
 var xhr = new XMLHttpRequest()
 
-console.log(xhr)
+xhr.onreadystatechange == function(){
+    if(xhr.readyState == 4){
+        console.log(xhr)
+    }
+}
+
+xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/1")
+
+xhr.send()
